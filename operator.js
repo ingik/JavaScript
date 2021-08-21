feec5399914704787aab61e38bc8e10053bc512d
ingik <skskoe@naver.com>
2021-08-21 19:46:16 +0900

    2021.08.21

---
+++ operator.js
@@ -0,0 +1,58 @@
+// 1. String concatenation
+console.log('my' + ' cat');
+console.log('1' + 2);
+console.log(`string literals: 1 + 2 = ${1 + 2}`);
+
+// 2. Numeric operators
+console.log(1 + 1); //add
+console.log(1 - 1); //substract
+console.log(1 / 1); //divide
+console.log(1 * 1); //multiply
+console.log(5 % 2); //remainder
+console.log(2 ** 3); //exponentiation
+
+// 3. Increment and  decrement operators
+let counter = 2;
+const preIncrement = ++counter;
+// counter = counter + 1;
+// preIncrement = counter;
+console.log(`preIncrement: ${preIncrement}, counter: ${counter}`);
+const postIncrement = counter++;
+// postIncrement = counter;
+// counter = counter + 1;
+
+// 4. Assignment operators
+let x = 3;
+let y = 6;
+x += y;
+x -= y;
+x *= y;
+x /= y;
+
+// 5. Comparison operators
+console.log(10 < 6);
+console.log(10 <= 6);
+console.log(10 > 6);
+console.log(10 >= 6);
+
+// 6. Logical operators: || (or), && (and), ! (not)
+const value1 = false;
+const value2 = 4 < 2;
+
+// || (or), finds the first truthy value
+console.log(`or: ${value1 || value2 || check()}`);
+
+// && (and), finds the first falsy value
+console.log(`and: ${value1 && value2 && check()}`);
+
+function check(){
+    for (let i = 0; i < 10; i++){
+        //wasting time
+        console.log('쓋');
+    }
+    return true;
+}
+
+// ! (not)  const value1: ture
+console.log(!value1);
+
--- variable.js
+++ variable.js
@@ -1,10 +1,11 @@
+
 //1. Use Strict
 //added in ES 5
 //use this for Valina Javascript
 'use strict';
 
 
-//2. Variable
+//2. Variable   rw(read/write)
 //let (added in ES6)
 
 let globalName = 'global name';
@@ -31,15 +32,24 @@ var age;
 console.log(age);
 
 
-//3. Consants
-//favor immutable data type always for a few reasons;
-// - security
-// - thread safety
-// - reduce human mistakes
+//3. Consant, r(read only)
+// use const whenever possible.
+// only use let if variable needs to change.
+
 const datsInWeek = 7;
 const maxNumber = 5;
 // mutable type -> let
 // immutable type -> const
+
+
+// Note!
+// Immutable data types: premitive types, frozen objects(i.e. object.freeze())
+// Mutable data types: all objects by default are muatable in JS
+// favor immutable data type always for a few reasons;
+// - security
+// - thread safety
+// - reduce human mistakes
+
 
 
 //4. Variable types
@@ -73,5 +83,39 @@ const helloBob = `hi ${brendan}`; //template literals (String)
 console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
 console.log('value: ' + helloBob + ' type: ' + typeof helloBob);
 
+//boolean
+//false: 0, null, undefinde, NaN, ''
+//ture : nay other value
+const canRead = true;
+const test = 3 < 1; // false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+//null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+//undefinded
+let x;
+console.log(`value: ${x}, type: ${typeof x}`);
+
+//symbol, create unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2);
+const symbol1 = gSymbol('id');
+const symbol2 = gSymbol('id');
+console.log(gSymbol1 === gSymbol2); //true
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1.description}`);
 
 
+//5. Dynamic typing : dynamically typed language
+let text = 'hello';
+console.log(text.charAt(0));
+console.log(`value: ${text}, type: ${typeof text}`);
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '7' + 5;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '8' /'2';
+console.log(`value: ${text}, type: ${typeof text}`);
